0ca23b4f7d723be8dd40b14faffd08ff6c9df881
ma472168 <ma472168@uaeh.edu.mx>
2026-04-13 01:34:12 -0600

    object-cover changed to objet-contain

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -74,7 +74,7 @@ export default function Home() {
                   <img
                     src={artwork.image_url}
                     alt={artwork.title}
-                    className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-105"
+                    className="w-full h-full object-contain transition-transform duration-700 group-hover:scale-105"
                     referrerPolicy="no-referrer"
                   />
                   <div className="absolute inset-0 bg-ink/20 opacity-0 group-hover:opacity-100 transition-opacity duration-500 flex items-center justify-center">
